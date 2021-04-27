b287b32b30ca6193d29d57d3e2d360dacc5b23c6
wjdtmddnr24 <wjdtmddnr24@naver.com>
2021-04-27 17:03:59 +0000

    created mentoring.service.ts

---
+++ src/services/mentoring.service.ts
@@ -0,0 +1,13 @@
+import { IMentoring } from '../interfaces/soma.interface';
+
+export async function addMentoring(mentoring: IMentoring): Promise<IMentoring> {
+  return null;
+}
+
+export async function getMentoringById(id: string): Promise<IMentoring> {
+  return null;
+}
+
+export async function getMostRecentMentoring(): Promise<IMentoring> {
+  return null;
+}
